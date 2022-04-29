9b71fbb588481deb8b98f7db86478c77cf541a60
Mac <contact@youssefbaroudi.com>
2022-04-29 22:32:17 +0000

    new terrraform files

---
+++ terraform/blobStorage.tf
@@ -0,0 +1,13 @@
+resource "azurerm_storage_account" "SA_App1_UAT" {
+  name                     = "SA_App1_UAT"
+  location                 = azurerm_resource_group.RG_App1_UAT.location
+  resource_group_name      = azurerm_resource_group.RG_App1_UAT.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "Container_Tfstate_App1_UAT" {
+  name                  = "Container_Tfstate_App1_UAT"
+  storage_account_name  = azurerm_storage_account.SA_App1_UAT.name
+  container_access_type = "private"
+}
