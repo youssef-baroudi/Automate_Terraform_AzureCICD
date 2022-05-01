1a35beac39fb0558a343df98bfd7f77bd906308f
Mac <contact@youssefbaroudi.com>
2022-05-01 22:27:52 +0000

    Valid version for creating Terraform infra using azure DevopsPipe

--- terraform/rg.tf
+++ terraform/rg.tf
@@ -1,18 +1,11 @@
-#resource "azurerm_resource_group" "RG_App1_UAT" {
- # name     = "RG_App1_UAT"
- # location = var.location
- # tags = {
-  #    env = "UAT"
- # }
-#}
-
 resource "azurerm_resource_group" "RG_App1_UAT" {
-  name     = "RG_App1_UAT_4Test"
+  name     = "RG_App1_UAT"
   location = var.location
   tags = {
       env = "UAT"
   }
 }
+
 output "RG4TestName" {
   value = azurerm_resource_group.RG_App1_UAT.name
 }
