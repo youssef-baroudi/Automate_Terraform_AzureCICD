621d16166eaa1141f011fd6cc982572bde7060e2
Mac <contact@youssefbaroudi.com>
2022-05-01 21:59:51 +0000

    for test

--- terraform/blobStorage.tf
+++ terraform/blobStorage.tf
@@ -1,13 +1,13 @@
-resource "azurerm_storage_account" "SA_App1_UAT" {
-  name                     = "storageaccountapp1uat"
-  location                 = azurerm_resource_group.RG_App1_UAT.location
-  resource_group_name      = azurerm_resource_group.RG_App1_UAT.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+#resource "azurerm_storage_account" "SA_App1_UAT" {
+ # name                     = "storageaccountapp1uat"
+  #location                 = azurerm_resource_group.RG_App1_UAT.location
+  #resource_group_name      = azurerm_resource_group.RG_App1_UAT.name
+  #account_tier             = "Standard"
+  #account_replication_type = "LRS"
+#}
 
-resource "azurerm_storage_container" "Container_Tfstate_App1_UAT" {
-  name                  = "containertfstateapp1uat"
-  storage_account_name  = azurerm_storage_account.SA_App1_UAT.name
-  container_access_type = "private"
-}
+#resource "azurerm_storage_container" "Container_Tfstate_App1_UAT" {
+ # name                  = "containertfstateapp1uat"
+  #storage_account_name  = azurerm_storage_account.SA_App1_UAT.name
+  #container_access_type = "private"
+#}
--- terraform/rg.tf
+++ terraform/rg.tf
@@ -1,7 +1,18 @@
+#resource "azurerm_resource_group" "RG_App1_UAT" {
+ # name     = "RG_App1_UAT"
+ # location = var.location
+ # tags = {
+  #    env = "UAT"
+ # }
+#}
+
 resource "azurerm_resource_group" "RG_App1_UAT" {
-  name     = "RG_App1_UAT"
+  name     = "RG_App1_UAT_4Test"
   location = var.location
   tags = {
       env = "UAT"
   }
+}
+output "RG4TestName" {
+  value = azurerm_resource_group.RG_App1_UAT.name
 }
