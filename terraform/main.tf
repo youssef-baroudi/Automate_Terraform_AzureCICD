32d397ba9b084e9bcd865671304ef49903102db8
Mac <contact@youssefbaroudi.com>
2022-05-01 18:33:13 +0000

    all ..v3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,6 @@
-#terraform {
-   # backend "azurerm" {
-       # resource_group_name  = azurerm_resource_group.RG_App1_UAT.name
-       # storage_account_name = azurerm_storage_account.SA_App1_UAT.name
-       # container_name       = azurerm_storage_container.Container_Tfstate_App1_UAT.name
-       # key                  = "terraform.tfstate"
-   # }
-
-#}
+terraform {
+   backend "local" {}
+}
 
 provider "azurerm" {
   features {}
